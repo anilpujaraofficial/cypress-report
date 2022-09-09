1df978734a93a58e38f851199565b480da96fa80
Anil Pujara <anilpujara125@gmail.com>
2022-09-09 11:58:33 +0545

    mattermost file update

--- cypress/helper/mattermost_report/mattermost.mjs
+++ cypress/helper/mattermost_report/mattermost.mjs
@@ -1,5 +1,8 @@
 import json_report from "../../reports/mochareports/report.json" assert { type: "json" };
 import fetch from "node-fetch";
+import dotenv from "dotenv";
+import chalk from "chalk";
+dotenv.config();
 
 var today = new Date();
 var dd = String(today.getDate()).padStart(2, "0");
@@ -8,7 +11,7 @@ var yyyy = today.getFullYear();
 
 today = mm + "/" + dd + "/" + yyyy;
 
-const text = `#### Test results for Aeon Automation as of ${today}\n@channel please review failed tests. \n\n| Test Cases Name  | Test Passed   | Test Failed  | Test Pending | Test Skipped | Test Duration |\n|:-----------|:-----------|:-----------|:-----------|:-----------|:-----------------|\n`;
+const text = `#### Test Results for ${process.env.PROJECT_NAME} as of ${today}\n@channel please review failed tests. \n\n| Test Cases Name  | Test Passed   | Test Failed  | Test Pending | Test Skipped | Test Duration |\n|:-----------|:-----------|:-----------|:-----------|:-----------|:-----------------|\n`;
 
 const arr = [];
 
@@ -25,24 +28,22 @@ json_report.results.map((data) => {
 });
 
 const final_result = text + arr.join("");
-console.log(final_result);
+// console.log(final_result);
 const messageBody = {
   text: final_result,
 };
 
 // this will hit api for webhooks and will send message about failed and passed test cases to mattermost channel.
 
-// let response = fetch(
-//   "https://ekbana.letsperk.com/hooks/7oo4ta4kotnwxfn691nzcj5hqe",
-//   {
-//     method: "POST",
-//     headers: {
-//       "Content-Type": "application/json;",
-//     },
-//     body: JSON.stringify(messageBody),
-//   }
-// );
+let response = fetch(process.env.MATTERMOST_URL, {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json;",
+  },
+  body: JSON.stringify(messageBody),
+});
 
 // let result = response;
 // console.log(result);
-console.log("success !!!");
+
+console.log(chalk.green("Execute Successfully !!!"));
